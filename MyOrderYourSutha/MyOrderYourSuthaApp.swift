864880d30e93289cfdd4504ac25a76478886e2d9
sutha06 <suthakaran.siva0601@gmail.com>
2024-10-10 18:08:22 -0400

    Initial Commit

---
+++ MyOrderYourSutha/MyOrderYourSuthaApp.swift
@@ -0,0 +1,20 @@
+//
+//  MyOrderYourSuthaApp.swift
+//  MyOrderYourSutha
+//
+//  Created by Suthakaran Siva on 2024-10-10.
+//
+
+import SwiftUI
+
+@main
+struct MyOrderYourSuthaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
